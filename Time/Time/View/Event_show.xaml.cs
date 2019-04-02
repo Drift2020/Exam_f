a0ecfffd66dbf51f62b101d3f46a225358b2fa18
Mamory <32417036+Drift2020@users.noreply.github.com>
2019-04-02 14:55:38 +0300

    Event alert

---
+++ Time/Time/View/Event_show.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Time.View
+{
+    /// <summary>
+    /// Interaction logic for Event_show.xaml
+    /// </summary>
+    public partial class Event_show : Window
+    {
+        public Event_show()
+        {
+            InitializeComponent();
+        }
+    }
+}
